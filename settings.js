c19c10b6da921b3ecf32e7efa1c0be8b35a3720f
mahesh45673 <saliyabotv@gmail.com>
2024-06-02 22:29:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -52,7 +52,7 @@ module.exports = {
      ONLY_COMMANDS : false,
      STATUS : false,
   },
-  BOT_OFFLINE : false,
+  BOT_OFFLINE : ture,
   INBOX_BLOCK : false,
   AUTO_REACT : false,
   OWNER_REACT : '🤵‍♂️',
